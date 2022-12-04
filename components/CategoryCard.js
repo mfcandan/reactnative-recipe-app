3992c12ecef43fd14215b911048c369048ee2a27
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-12-04 14:41:10 +0300

    RecipeCard

---
+++ components/CategoryCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { COLORS, FONTS, SIZES } from "../constants";
+
+const CategoryCard = ({ containerStyle, categoryItem, onPress }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+        marginTop: 10,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.gray2,
+      }}
+    >
+      <Image
+        source={categoryItem.image}
+        resizeMode="cover"
+        style={{ width: 100, height: 100, borderRadius: SIZES.radius }}
+      />
+      <View style={{ width: "65%", paddingHorizontal: 20 }}>
+        <Text style={{ flex: 1, ...FONTS.h2 }}>{categoryItem.name}</Text>
+        <Text style={{ color: COLORS.gray, ...FONTS.body4 }}>
+          {categoryItem.duration} | {categoryItem.serving} Serving{" "}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CategoryCard;
--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React from "react";
 import {
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native';
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+} from "react-native";
+import CategoryCard from "../components/CategoryCard";
+import { COLORS, dummyData, SIZES } from "../constants";
 
 const Home = ({ navigation }) => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Recipe")}
-            >
-                <Text>Navigate to Recipe</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+      <FlatList
+        data={dummyData.categories}
+        keyExtractor={(item) => `${item.id}`}
+        keyboardDismissMode="on-drag"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={<View></View>}
+        renderItem={({ item }) => {
+          return (
+            <CategoryCard
+              onPress={() => navigation.navigate("Recipe", { recipe: item })}
+              containerStyle={{ marginHorizontal: SIZES.padding }}
+              categoryItem={item}
+            />
+          );
+        }}
+        ListFooterComponent={<View style={{ marginBottom: 100 }} />}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default Home;
