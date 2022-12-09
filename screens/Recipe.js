b49ea5e870f42384494c20769ffdf4a25c26b2df
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-12-09 15:27:28 +0300

    Animated Flatlist

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,21 +1,113 @@
-import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, Animated, Image } from "react-native";
+import { COLORS, FONTS } from "../constants";
 
-const Recipe = () => {
+const HEADER_HEIGHT = 350;
+
+const Recipe = ({ navigation, route }) => {
+  const [selectedRecipe, setSelectedRecipe] = useState();
+
+  const scrollY = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    let { recipe } = route.params;
+    setSelectedRecipe(recipe);
+  }, []);
+
+  const renderRecipeCardHeader = () => {
     return (
-        <View
+      <View
+        style={{
+          alignItems: "center",
+          overflow: "hidden",
+          marginTop: -1000,
+          paddingTop: 1000,
+        }}
+      >
+        {/* BACKGROUND IMAGE */}
+        <Animated.Image
+          source={selectedRecipe?.image}
+          resizeMode="contain"
+          style={{
+            height: HEADER_HEIGHT,
+            width: "200%",
+            transform: [
+              {
+                translateY: scrollY.interpolate({
+                  inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                  outputRange: [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75],
+                }),
+              },
+              {
+                scale: scrollY.interpolate({
+                  inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                  outputRange: [2, 1, 0.75],
+                }),
+              },
+            ],
+          }}
+        />
+        {/* RECIPE CREATOR CARD */}
+      </View>
+    );
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      <Animated.FlatList
+        data={selectedRecipe?.ingredients}
+        keyExtractor={(item) => `${item.id}`}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={<View>{renderRecipeCardHeader()}</View>}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        renderItem={({ item }) => (
+          <View
             style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+              flexDirection: "row",
+              paddingHorizontal: 30,
+              marginVertical: 5,
             }}
-        >
-            <Text style={{fontSize:50}}>RECIPE</Text>
-        </View>
-    )
-}
+          >
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                height: 50,
+                width: 50,
+                borderRadius: 5,
+                backgroundColor: COLORS.lightGray,
+              }}
+            >
+              <Image source={item?.icon} style={{ width: 40, height: 40 }} />
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                paddingHorizontal: 20,
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ ...FONTS.body3 }}>{item?.description}</Text>
+            </View>
+
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text style={{ ...FONTS.body3 }}>{item?.quantity}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
 
 export default Recipe;
